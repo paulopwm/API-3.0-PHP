db25722e5426ec54df45094695e19fdb832cd587
Paulo M. Santos <paulomobbssantos@gmail.com>
2021-06-26 15:19:44 -0300

    Ajuste na análise de fraude;

--- src/Cielo/API30/Ecommerce/FraudAnalysis.php
+++ src/Cielo/API30/Ecommerce/FraudAnalysis.php
@@ -92,6 +92,12 @@ class FraudAnalysis implements \JsonSerializable, CieloSerializable
     /** @var string $casePriority */
     private $casePriority;
 
+    /** @var string $id */
+    private $id;
+
+    /** @var int $fraudAnalysisReasonCode */
+    private $fraudAnalysisReasonCode;
+
     /**
      * Fraud Analysis constructor.
      *
@@ -115,26 +121,17 @@ class FraudAnalysis implements \JsonSerializable, CieloSerializable
     public function populate(\stdClass $data)
     {
 
-        $this->provider             = isset($data->Provider) ? $data->Provider : null;
-        $this->status               = isset($data->Status) ? $data->Status : null;
-        $this->sequence             = isset($data->Sequence) ? $data->Sequence : null;
-        $this->sequenceCriteria     = isset($data->SequenceCriteria) ? $data->SequenceCriteria : null;
-        $this->captureOnLowRisk     = isset($data->CaptureOnLowRisk) ? $data->CaptureOnLowRisk : null;
-        $this->voidOnHighRisk       = isset($data->VoidOnHighRisk) ? $data->VoidOnHighRisk : null;
-        $this->totalOrderAmount     = isset($data->TotalOrderAmount) ? $data->TotalOrderAmount : null;
-        $this->fingerPrintId        = isset($data->FingerPrintId) ? $data->FingerPrintId : null;
-        $this->addressInfoCode      = isset($data->AddressInfoCode) ? $data->AddressInfoCode : null;
-        $this->factorCode           = isset($data->FactorCode) ? $data->FactorCode : null;
-        $this->score                = isset($data->Score) ? $data->Score : null;
-        $this->binCountry           = isset($data->BinCountry) ? $data->BinCountry : null;
-        $this->cardIssuer           = isset($data->CardIssuer) ? $data->CardIssuer : null;
-        $this->cardScheme           = isset($data->CardScheme) ? $data->CardScheme : null;
-        $this->hostSeverity         = isset($data->HostSeverity) ? $data->HostSeverity : null;
-        $this->internetInfoCode     = isset($data->InternetInfoCode) ? $data->InternetInfoCode : null;
-        $this->ipRoutingMethod      = isset($data->IpRoutingMethod) ? $data->IpRoutingMethod : null;
-        $this->scoreModelUsed       = isset($data->ScoreModelUsed) ? $data->ScoreModelUsed : null;
-        $this->casePriority         = isset($data->CasePriority) ? $data->CasePriority : null;
-        
+        $this->provider                 = isset($data->Provider) ? $data->Provider : null;
+        $this->id                       = isset($data->Id) ? $data->Id : null;
+        $this->status                   = isset($data->Status) ? $data->Status : null;
+        $this->fraudAnalysisReasonCode  = isset($data->FraudAnalysisReasonCode) ? $data->FraudAnalysisReasonCode : null;
+        $this->sequence                 = isset($data->Sequence) ? $data->Sequence : null;
+        $this->sequenceCriteria         = isset($data->SequenceCriteria) ? $data->SequenceCriteria : null;
+        $this->captureOnLowRisk         = isset($data->CaptureOnLowRisk) ? $data->CaptureOnLowRisk : null;
+        $this->voidOnHighRisk           = isset($data->VoidOnHighRisk) ? $data->VoidOnHighRisk : null;
+        $this->totalOrderAmount         = isset($data->TotalOrderAmount) ? $data->TotalOrderAmount : null;
+        $this->fingerPrintId            = isset($data->FingerPrintId) ? $data->FingerPrintId : null;
+
         if (isset($data->Browser)) {
             $this->browser = new Browser();
             $this->browser->populate($data->Browser);
@@ -147,11 +144,9 @@ class FraudAnalysis implements \JsonSerializable, CieloSerializable
 
         if (isset($data->MerchantDefinedFields)) {
             foreach($data->MerchantDefinedFields as $merchantDefinedField){
-            
                 $merchantDefinedFieldsInstance = new MerchantDefinedFields();
                 $merchantDefinedFieldsInstance->populate($merchantDefinedField);
-
-                $this->merchantDefinedFields [] = $merchantDefinedFieldsInstance;
+                $this->merchantDefinedFields[] = $merchantDefinedFieldsInstance;
             }
         }
 
@@ -458,226 +453,6 @@ class FraudAnalysis implements \JsonSerializable, CieloSerializable
     public function setReplyData($replyData)
     {
         $this->replyData = $replyData;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getAddressInfoCode()
-    {
-        return $this->addressInfoCode;
-    }
-
-    /**
-     * @param $addressInfoCode
-     *
-     * @return $this
-    */
-    public function setAddressInfoCode($addressInfoCode)
-    {
-        $this->addressInfoCode = $addressInfoCode;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getFactorCode()
-    {
-        return $this->factorCode;
-    }
-
-    /**
-     * @param $factorCode
-     *
-     * @return $this
-    */
-    public function setFactorCode($factorCode)
-    {
-        $this->factorCode = $factorCode;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getScore()
-    {
-        return $this->score;
-    }
-
-    /**
-     * @param $score
-     *
-     * @return $this
-    */
-    public function setScore($score)
-    {
-        $this->score = $score;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getBinCountry()
-    {
-        return $this->binCountry;
-    }
-
-    /**
-     * @param $binCountry
-     *
-     * @return $this
-    */
-    public function setBinCountry($binCountry)
-    {
-        $this->binCountry = $binCountry;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getCardIssuer()
-    {
-        return $this->cardIssuer;
-    }
-
-    /**
-     * @param $cardIssuer
-     *
-     * @return $this
-    */
-    public function setCardIssuer($cardIssuer)
-    {
-        $this->cardIssuer = $cardIssuer;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getCardScheme()
-    {
-        return $this->cardScheme;
-    }
-
-    /**
-     * @param $cardScheme
-     *
-     * @return $this
-    */
-    public function setCardScheme($cardScheme)
-    {
-        $this->cardScheme = $cardScheme;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getHostSeverity()
-    {
-        return $this->hostSeverity;
-    }
-
-    /**
-     * @param $hostSeverity
-     *
-     * @return $this
-    */
-    public function setHostSeverity($hostSeverity)
-    {
-        $this->hostSeverity = $hostSeverity;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getInternetInfoCode()
-    {
-        return $this->internetInfoCode;
-    }
-
-    /**
-     * @param $internetInfoCode
-     *
-     * @return $this
-    */
-    public function setInternetInfoCode($internetInfoCode)
-    {
-        $this->internetInfoCode = $internetInfoCode;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getIpRoutingMethod()
-    {
-        return $this->ipRoutingMethod;
-    }
-
-    /**
-     * @param $ipRoutingMethod
-     *
-     * @return $this
-    */
-    public function setIpRoutingMethod($ipRoutingMethod)
-    {
-        $this->ipRoutingMethod = $ipRoutingMethod;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getScoreModelUsed()
-    {
-        return $this->scoreModelUsed;
-    }
-
-    /**
-     * @param $scoreModelUsed
-     *
-     * @return $this
-    */
-    public function setScoreModelUsed($scoreModelUsed)
-    {
-        $this->scoreModelUsed = $scoreModelUsed;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-    */
-    public function getCasePriority()
-    {
-        return $this->casePriority;
-    }
-
-    /**
-     * @param $casePriority
-     *
-     * @return $this
-    */
-    public function setCasePriority($casePriority)
-    {
-        $this->casePriority = $casePriority;
 
         return $this;
     }
